d79ae8d686d2ce0c7c3d608043ddacdf752ffdc3
Huzaifa Banegar <huzaifabanegar@Huzaifas-MacBook-Air.local>
2024-02-24 16:45:20 +0530

    fix some styling

--- client-side/src/utils/constant.js
+++ client-side/src/utils/constant.js
@@ -4,6 +4,6 @@ export const CAROUSAL1_url="http://quinnlo2003.files.wordpress.com/2013/06/home-
 
 export const CAROUSAL2_url="https://www.decorilla.com/online-decorating/wp-content/uploads/2016/07/find-an-interior-designer.jpg"
 
-export const CAROUSAL3_url="https://thearchitectsdiary.com/wp-content/uploads/2017/09/Indian-Interior-Design-20.jpg"
+export const CAROUSAL3_url="https://i.pinimg.com/originals/ea/56/ad/ea56ad3cb70823c47cdac40a27916446.jpg"
 
 export const CDN_url="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
