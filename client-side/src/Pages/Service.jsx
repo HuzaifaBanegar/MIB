ac02623173f01f2e186e2ca75eff80b38cb1b827
Huzaifa Banegar <huzaifabanegar@Huzaifas-MacBook-Air.local>
2024-02-24 14:53:08 +0530

    fix some styling

--- client-side/src/Pages/Service.jsx
+++ client-side/src/Pages/Service.jsx
@@ -16,21 +16,43 @@ export const Service=()=>{
                     <p className="desc1" style={{textAlign:'justify', marginTop:"3px"}}> With expertise in interior design, architecture, and exterior aesthetics, we transform spaces into captivating masterpieces. Our innovative designs harmonize beauty and practicality, crafting environments that inspire and elevate. Experience the art of architectural excellence with us.</p>
                 </Box>
             </div>
+            <h2 className="head1"  marginX={'auto'}>Our Design Philosophy</h2>
             <div className='service-icons'>
+                
                 <Paper className='service-papers' elevation={3} >
                     <Interior style={{margin:'0 auto'}} />
-                    <h2 className='head1'>Interior Planning</h2>
-                    <p className='desc1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dolorem veniam, dolores itaque laborum sunt doloribus, at voluptatem ex odio deserunt, impedit aliquid aperiam necessitatibus eos fuga commodi unde eligendi!</p>
+                    <h2 className='head1'>Collaboration</h2>
+                    <p className='desc1'>
+                        We partner closely with
+                        our clients to understand
+                        their unique vision and
+                        needs, and to incorporate
+                        their personal style into
+                        each design.
+                    </p>
                 </Paper>
                 <Paper className='service-papers' elevation={3} >
-                    <Arch style={{margin:'0 auto'}}/>
-                    <h2 className='head1'>Architecture</h2>
-                    <p className='desc1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dolorem veniam, dolores itaque laborum sunt doloribus, at voluptatem ex odio deserunt, impedit aliquid aperiam necessitatibus eos fuga commodi unde eligendi!</p>
+                    <Arch style={{margin:'0 auto', width:'100px',height:'100px'}}/>
+                    <h2 className='head1'>Functionality</h2>
+                    <p className='desc1'>
+                        We prioritize functional
+                        design that enhances our
+                        clients' daily lives, ensuring
+                        that every space is both
+                        beautiful and livable.
+                    </p>
                 </Paper>
                 <Paper className='service-papers' elevation={3} >
-                    <Exterior style={{margin:'0 auto'}}/>
-                    <h2 className='head1'>Exterior</h2>
-                    <p className='desc1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dolorem veniam, dolores itaque laborum sunt doloribus, at voluptatem ex odio deserunt, impedit aliquid aperiam necessitatibus eos fuga commodi unde eligendi!</p>
+                    <Exterior style={{margin:'0 auto',  width:'100px',height:'100px'}}/>
+                    <h2 className='head1'>Eco-Friendliness</h2>
+                    <p className='desc1'>
+                        We strive to incorporate
+                        sustainable design
+                        strategies and materials
+                        into each of our projects
+                        for a healthier, more eco -
+                        friendly future.
+                    </p>
                 </Paper>
                 
 
